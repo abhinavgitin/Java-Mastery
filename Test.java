37018239d8fc89e16e8745003bba2485396130f2
abhinavgitin <prabhudarsanpuri987@gmail.com>
2025-07-04 02:48:17 +0530

    add all files privately

---
+++ Test.java
@@ -0,0 +1,306 @@
+// class Test {
+//     public static void main (String args []){
+
+//         int factorial = 1;
+//         for (int i = 5 ; i > 0 ; i--){
+//             factorial = factorial * i;
+//         }
+//         System.out.println(factorial);
+//     }
+// }
+// User function Template for Java
+
+// class Solution {
+//     public static void main(String[] args) {
+//         System.out.println(revStr("abhinav"));
+//     }
+
+//     static String revStr(String s) {
+//         // code here
+//         int length = s.length();
+//         int i = 0;
+//         String str = "";
+//         while (i < s.length() - 1) {
+//             str += s.charAt(length);
+//             length--;
+//             i++;
+//         }
+//         return str;
+//     }
+// }
+// class Solution {
+//     public static void main(String[] args) {
+//         System.out.println(isPrime(21));
+//     }
+//     public static String isPrime(int n) {
+//         // code here
+//         String str ="";
+//         if ( n % 2 == 0){
+//             str = "Yes";
+//         } else {
+//             str = "No";
+//         }
+//         return str;
+//     }
+// }
+
+// User function Template for Java
+//                                                    this is the demo of the sort 
+// class Test {
+//     public static void commonElements(int a[], int b[]) {
+//         // Your code here
+//         int length = 0;
+//         if (a.length > b.length) {
+//             for (int i = 0; i < a.length; i++) {
+//                 for (int j = 0; j < b.length; j++) {
+//                     if (a[i] == b[j]) {
+//                         length += 1;
+
+//                     }
+//                 }
+//             }
+//         } else {
+//             for (int i = 0; i < a.length; i++) {
+//                 for (int j = 0; j < b.length; j++) {
+//                     if (a[i] == b[j]) {
+//                         length += 1;
+//                     }
+//                 }
+//             }
+//         }
+//         int arr[] = new int[length];
+
+//         if (a.length > b.length) {
+//             for (int i = 0; i < a.length; i++) {
+//                 for (int j = 0; j < b.length; j++) {
+//                     if (a[i] == b[j]) {
+//                         arr[i] = b[j];
+
+//                     }
+//                 }
+//             }
+//         } else {
+//             for (int i = 0; i < a.length; i++) {
+//                 for (int j = 0; j < b.length; j++) {
+//                     if (a[i] == b[j]) {
+//                         arr[i] = b[j];
+//                     }
+//                 }
+//             }
+//         }
+
+//         for (int m = 0; m < arr.length; m++) {
+//             for (int n = 0; n < arr.length - m - 1; n++) {
+//                 if (arr[n] > arr[n + 1]) {
+//                     int temp = arr[n];
+//                     arr[n] = arr[n + 1];
+//                     arr[n + 1] = temp;
+//                 }
+//             }
+//         }
+//         for (int p = 0; p < arr.length; p++) {
+//             if (arr[p] != 0) {
+//                 System.out.print(arr[p] + " ");
+//             }
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         int a[] = { 2, 2, 4, 5, 6, 6, 7 };
+//         int b[] = { 2, 2, 6, 6, 7,5,5,4,66,0,3 };
+//         commonElements(a, b);
+
+//     }
+// }
+
+// import java.util.ArrayList;
+
+// class Solution {
+//     public static ArrayList<Integer> commonElements(int a[], int b[]) {
+//         int length = 0;
+//         if (a.length > b.length) {
+//             for (int i = 0; i < a.length; i++) {
+//                 for (int j = 0; j < b.length; j++) {
+//                     if (a[i] == b[j]) {
+//                         length += 1;
+//                     }
+//                 }
+//             }
+//         } else {
+//             for (int i = 0; i < a.length; i++) {
+//                 for (int j = 0; j < b.length; j++) {
+//                     if (a[i] == b[j]) {
+//                         length += 1;
+//                     }
+//                 }
+//             }
+//         }
+
+//         int arr[] = new int[length];
+
+//         if (a.length > b.length) {
+//             for (int i = 0; i < a.length; i++) {
+//                 for (int j = 0; j < b.length; j++) {
+//                     if (a[i] == b[j]) {
+//                         arr[i] = b[j];
+//                     }
+//                 }
+//             }
+//         } else {
+//             for (int i = 0; i < a.length; i++) {
+//                 for (int j = 0; j < b.length; j++) {
+//                     if (a[i] == b[j]) {
+//                         arr[i] = b[j];
+//                     }
+//                 }
+//             }
+//         }
+
+//         for (int m = 0; m < arr.length; m++) {
+//             for (int n = 0; n < arr.length - m - 1; n++) {
+//                 if (arr[n] > arr[n + 1]) {
+//                     int temp = arr[n];
+//                     arr[n] = arr[n + 1];
+//                     arr[n + 1] = temp;
+//                 }
+//             }
+//         }
+
+//         ArrayList<Integer> result = new ArrayList<>();
+//         for (int p = 0; p < arr.length; p++) {
+//             if (arr[p] != 0) {
+//                 result.add(arr[p]);
+//             }
+//         }
+//         return result;
+//     }
+
+//     public static void main(String[] args) {
+//         int a[] = {2, 2, 4, 5, 6, 6, 7};
+//         int b[] = {2, 2, 6, 6, 7, 5, 5, 4, 66, 0, 3};
+//         ArrayList<Integer> output = commonElements(a, b);
+//         for (int val : output) {
+//             System.out.print(val + " ");
+//         }
+//     }
+// }
+
+// class Solution {
+//     public static void main(String[] args) {
+//         int arrr[]={1,2,3,4,5};
+//         System.out.println(average(arrr));
+//     }
+//     public static String average(int arr[]) {
+//         // code here
+//         double sum = 0;
+//         for ( int i = 0 ; i < arr.length ; i++){
+//             sum = sum+arr[i];
+//         }
+//         double avg = sum / arr.length;
+//         String str = avg+"";
+//         return str;
+//     }
+//}
+
+// // User function template for Java
+// class Geeks {
+//     public static void main(String[] args) {
+//         System.out.println(follPatt("xyyx"));
+//     }
+
+//     static int follPatt(String s) {
+//         // Your code here
+//         char first_char = s.charAt(0);
+//         char second_char = '\0';
+//         if (s.length() == 1) {
+//             return 0;
+//         } // default value of char
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s.charAt(i) != first_char) {
+//                 second_char = s.charAt(i);
+//                 break;
+//             }
+//         }
+//         if (second_char == '\0')
+//             return 0;
+//         int countX = 0;
+//         int countY = 0;
+//         for (int j = 0; j < s.length(); j++) {
+//             if (s.charAt(j) == first_char) {
+//                 countX++;
+//             } else if (s.charAt(j) == second_char) {
+//                 countY++;
+//             } else {
+//                 return 0;
+//             }
+//         }
+//         return countX == countY ? 1 : 0;
+//     }
+// }
+// User function Template for Java
+// class Solution {
+//     public static void main(String[] args) {
+//         square(4);
+//     }
+//     public static void square(int s) {
+//         // Complete the code given below
+//         for ( int i = 1 ; i <= s ; i++){
+//             for ( int j = 1 ; j <= s ; j++){
+//                 if ( i == 1 || i == s || j == 1 || j == s){
+//                     System.out.print("*");
+//                 } else {
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+// // Your code here
+// if (s1.length() >= s2.length()) {
+//     for (int i = 0; i <= s1.length(); i++) {
+//         if (i < s2.length()) {
+//             if (s1.charAt(i) != (s2.charAt(i))) {
+//                 delete++;
+//             }
+//         }
+
+//     }
+//     delete = delete + (s1.length() - s2.length());
+// } else {
+//     for (int i = 0; i < s2.length(); i++) {
+//         if (i < s1.length()) {
+//             if (s1.charAt(i) != (s2.charAt(i))) {
+//                 delete++;
+//             }
+//         }
+//     }
+//     delete = delete + (s2.length() - s1.length());
+// }
+
+// return delete;
+class Test {
+
+    public static void main(String[] args) {
+        System.out.println(coutChars(
+                "cgudllrbrjlgvxuheeuwpewiylhnthzprofdxysmsgwkcztglzedalvcgostfavcmxpcvyhzpqhffojqqytooathdjfxyezuhbpu",
+                "dniciuehstodlqxhhcydvccfpbbpczqdiczfwhsgdbvrgewfyajbahsicalrxhavovgxdwkinrcmwpmpxurkuxtrfdpencvkvxvs"));
+    }
+
+    public static int coutChars(String s1, String s2) {
+        int char1[] = new int[26];
+        int char2[] = new int[26];
+        
+        for ( char c : s1.toCharArray()){
+            char1[c-'a']++;
+        }
+        for ( char c : s2.toCharArray()){
+            char2[c-'a']++;
+        }
+        int deletation =  0;
+        for ( int i = 0 ; i < 26 ; i++){
+            deletation+= Math.abs(char1[i]-char2[i]);
+        }
+        return deletation;
+    }
+}
---
+++ recursion2.java
@@ -0,0 +1,152 @@
+// public class recursion2 {
+//     public static void main(String[] args) {
+//         towerOfHanoi(3, "Source", "helper", "Destination");
+//     }
+//     public static void towerOfHanoi(int disks , String src , String helper , String destination){
+//         if  ( disks == 1 ){
+//             System.out.println("transfer of disk "+disks+" from "+src+" to "+destination);
+//             return;
+//         }
+//         towerOfHanoi(disks-1, src, destination, helper); // source se helper tak le jana hai
+//         System.out.println("transfer of disk "+disks+" from "+src+" to "+destination);
+//         towerOfHanoi(disks-1, helper, src, destination);
+//         return;
+//     }
+// }
+// class recursion2 {
+//     // time complexity = O(n) where n is the length of the string
+//     public static void main(String[] args) {
+//         reverseStr("Abhinav", 0);
+//     }
+
+//     public static void reverseStr(String str , int index) {
+//         if ( str.length()-1 == index ){
+//             System.out.print(str.charAt(index));
+//             return;
+//         }
+//         reverseStr(str, index+1);
+//         System.out.print(str.charAt(index));
+//     }
+// }
+// first and the last occurance
+
+// class recursion2 {
+//     public static int first = -1;
+//     public static int last = -1;
+
+//     public static void main(String[] args) {
+//         occurance("aabbcctteaaab", 0, 'b');
+
+//     }
+
+//     public static void occurance(String str, int index, char chr) {
+//         if (index == str.length()) {
+//             System.out.println("first occurance : " + first + " and the last occurance : " + last);
+//             return;
+//         }
+//         if (str.charAt(index) == chr) {
+//             if (first == -1) {
+//                 first = index;
+//             } else {
+//                 last = index;
+//             }
+//         }
+//         occurance(str, index + 1, chr);
+//     }
+// }
+
+// class recursion {
+//     public static int first = -1 ;
+//     public static int last = -1 ;
+
+//     public static void main(String[] args) {
+//         reoccurance("abhinav", 'a', 0);
+//         System.out.println("the first occurance if the char "+(first+1)+" and the last occurance is "+(last+1));
+//     }
+//     public static void reoccurance (String str , char chr , int index){
+//         if ( str.length()-1 == index){
+//             return;
+//         }
+//         if ( first == -1 ){
+//             first = index;
+//         } else {
+//             last = index;
+//         }
+//         reoccurance(str, chr, index+1);
+//     }
+// // }
+
+// class recursion {
+//     public static StringBuilder tempx = new StringBuilder();
+//     public static StringBuilder tempstr = new StringBuilder();
+//     public static void main ( String args []){
+//         shiftChar("axxaaxabhi", 'x', 0);
+//     }
+//     public static void shiftChar( String str , char chr , int index){
+
+//         if ( str.length() == index){
+//             System.out.println(tempstr+""+tempx);
+//             return;
+//         }
+//         if ( str.charAt(index) == chr){
+//             tempx.append(str.charAt(index));
+//         } else {
+//             tempstr.append(str.charAt(index));
+//         }
+
+//         shiftChar(str, chr, index+1);
+//     }
+// }
+
+// class recursion2 {
+//     public static void main ( String args []){
+//         int arr[] = {1,2,3,4,33,44,55,55};
+//         System.out.println(checksort(arr, 0));
+//     }
+//     public static boolean checksort( int arr[] , int index ){
+//         if ( arr.length-1 == index ){
+//             return true;
+//         }
+//         if ( arr[index] >= arr[index+1]){
+//             return false;
+//         }
+//         return checksort(arr, index+1);
+//     }
+//}
+// class recursion2 {
+//     public static StringBuilder newStr = new StringBuilder();
+
+//     public static void main(String[] args) {
+//         boolean map[] = new boolean[26];
+//         removeStr("aabhinnavvppuriii", map, 0);
+//     }
+
+//     public static void removeStr(String str, boolean map[], int index) {
+//         if (str.length() == index) {
+//             System.out.println(newStr);
+//             return;
+//         }
+//         char currentChar = str.charAt(index);
+//         if (map[currentChar - 'a'] == false) {
+//             newStr.append(currentChar);
+//             map[currentChar - 'a'] = true;
+//             removeStr(str, map, index + 1);
+//         } else {
+//             removeStr(str, map, index + 1);
+//         }
+//     }
+// }
+class recursion2 {
+    public static void main(String[] args) {
+        subsequesces("abc", 0, "");
+    }
+    public static void subsequesces ( String str, int index , String newStr){
+        if (str.length() == index){
+            System.out.println(newStr);
+            return;
+        }
+        char currentChar = str.charAt(index);
+        subsequesces(str, index+1, newStr+currentChar); // 1st wala
+        subsequesces(str, index+1, newStr); // 2nd wala
+    }
+}
