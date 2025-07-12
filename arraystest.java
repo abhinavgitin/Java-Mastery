7206a9d01b365535672653a77cbff86358b9225e
abhinavgitin <prabhudarsanpuri987@gmail.com>
2025-07-13 05:02:05 +0530

    add a file for the gfg160!

---
+++ arraystest.java
@@ -0,0 +1,47 @@
+public class arraystest {
+    public static void main(String[] args) {
+        int arr[] = {10,10,10,10,10 };
+        System.out.println("the second largest element is " + secondlargest(arr));
+    }
+
+    public static int secondlargest(int[] arr) {
+         int largest = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+
+            if (largest < arr[i]) {
+                largest = arr[i];
+
+            }
+        }
+        boolean allfound = false;
+    
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] == largest) {
+                arr[j] = 0;
+            }
+        }
+        for ( int m = 0 ; m < arr.length; m++){
+
+            if (arr[m] == 0) {
+                allfound = true;
+            }else {
+                allfound = false;
+                break;
+            }
+        }        
+        
+        
+        if (allfound) {
+            return -1; // if all elements are same, return -1
+        } else {
+            int secondLargest = arr[0];
+            for (int k = 0; k < arr.length; k++) {
+
+                if (secondLargest < arr[k]) {
+                    secondLargest = arr[k];
+                }
+            }
+            return secondLargest;
+        }
+    }
+}
