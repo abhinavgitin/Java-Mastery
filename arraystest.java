fafe34b62eb378a40f9c42b5f0c40c2757cb0456
abhinavgitin <prabhudarsanpuri987@gmail.com>
2025-07-18 01:22:05 +0530

    need to debug day 5 asap

--- arraystest.java
+++ arraystest.java
@@ -91,39 +91,90 @@
 // }
 
 // reverse of an array gfg 3
-class arraytest {
-    public void reverseArray(int arr[]) {
-        // code here
-        int index = 0;
-        for ( int i = 0; i <= (arr.length-1)/2 ; i++){
-            index = arr[i];
-            arr[i] = arr[(arr.length-1)-i];
-            arr[(arr.length-1)-i] = index;
-        }
-    }
-}
+// class arraytest {
+//     public void reverseArray(int arr[]) {
+//         // code here
+//         int index = 0;
+//         for ( int i = 0; i <= (arr.length-1)/2 ; i++){
+//             index = arr[i];
+//             arr[i] = arr[(arr.length-1)-i];
+//             arr[(arr.length-1)-i] = index;
+//         }
+//     }
+// }
 
 // the next gfg dat 4 is to be done 
+// class arraystest {
+//     public static void main(String[] args) {
+//         int arr[] = { 1, 2, 3, 4, 5 };
+//         int d = 2; // Number of positions to rotate
+//         reverseArray(arr, d);
+//     }
+//     public static void reverseArray( int arr[], int d){
+//         int n = arr.length;
+//         d = d % n; // Handle cases where d >= n
+//         int [] temp = new int[n];
+
+//         for ( int i = d; i < n; i++){
+//             temp[i - d] = arr[i];
+//         }
+//         for ( int i = 0; i < d; i++){
+//             temp[n - d + i] = arr[i];
+//         }
+//         for (int i = 0; i < n; i++) {
+//             System.out.print(temp[i] + " ");
+//         }
+//         System.out.println();
+//     }
+// }
+
+//gfg day 5 the next permutation of an array
 class arraystest {
     public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 4, 5 };
-        int d = 2; // Number of positions to rotate
-        reverseArray(arr, d);
-    }
-    public static void reverseArray( int arr[], int d){
-        int n = arr.length;
-        d = d % n; // Handle cases where d >= n
-        int [] temp = new int[n];
+        int arr[] = { 2,4,1,7,5,0};
+        nextPermutation(arr);
 
-        for ( int i = d; i < n; i++){
-            temp[i - d] = arr[i];
+    }
+
+    public static void nextPermutation(int arr[]) {
+        int n = arr.length;
+        int index = -1;
+        boolean foundThePermutation = false;
+        int justLarger = -1;
+        int largestNumber = -1;
+        for (int i = n - 1; i > 0; i--) {
+            if (!(arr[i] > arr[i - 1])) {
+                foundThePermutation = true;
+                index = i;
+                for (int j = index; j < n; j++) {
+                    if (arr[j] > arr[i]) {
+                        largestNumber = arr[j];
+                    }
+                    if (arr[j] > arr[i] && arr[j] < largestNumber) {
+                        justLarger = arr[j];
+                        break;
+                    }
+                }
+            }
+
         }
-        for ( int i = 0; i < d; i++){
-            temp[n - d + i] = arr[i];
+
+        if (foundThePermutation) {
+            arr[index] = justLarger;
+            for (int i = index + 1; i < (arr.length - 1) / 2; i++) {
+                int temp = arr[i];
+                arr[i] = arr[arr.length - 1 - i];
+                arr[arr.length - 1 - i] = temp;
+            }
+        } else {
+            for (int i = 0; i < arr.length / 2; i++) {
+                int temp = arr[i];
+                arr[i] = arr[arr.length - 1 - i];
+                arr[arr.length - 1 - i] = temp;
+            }
         }
-        for (int i = 0; i < n; i++) {
-            System.out.print(temp[i] + " ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
-        System.out.println();
     }
 }
