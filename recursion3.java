e79021d2ad734c97483a1c05f965e9d9e97dddb5
abhinavgitin <prabhudarsanpuri987@gmail.com>
2025-08-01 22:34:18 +0530

    solve the recursion

--- recursion3.java
+++ recursion3.java
@@ -1,14 +1,31 @@
-public class recursion3 {
+// public class recursion3 {
+//     public static void main(String[] args) {
+//         int[] arr = {1, 2, 3, 4, 5};
+//         int sum = arraySum(arr, arr.length - 1);
+//         System.out.println("Sum of array elements: " + sum);
+//     }
+//     public static int arraySum(int[] arr, int idx) {
+//         // code here
+//         if (idx < 0) {
+//             return 0;
+//         }
+//         return arr[idx] + arraySum(arr, idx - 1);
+//     }
+// }
+class recursion3 {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        int sum = arraySum(arr, arr.length - 1);
-        System.out.println("Sum of array elements: " + sum);
+        String str = "abc";
+        permutationOfStr(str, "");
     }
-    public static int arraySum(int[] arr, int idx) {
-        // code here
-        if (idx < 0) {
-            return 0;
+    public static void permutationOfStr( String str , String permutation ){
+        if ( str.length() == 0 ){
+            System.out.println(permutation);
+            return;
         }
-        return arr[idx] + arraySum(arr, idx - 1);
+        for ( int i = 0 ; i < str.length() ; i++ ){
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i+1);
+            permutationOfStr(newStr, permutation + currChar);
+        }
     }
 }
