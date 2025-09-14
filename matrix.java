3a0c7b6391dfb5afb7dfb936f18f48796c8c8aca
abhinavgitin <prabhudarsanpuri@gmail.com>
2025-09-14 16:00:43 +0530

    add one new file

---
+++ matrix.java
@@ -0,0 +1,12 @@
+class Matrix {
+    public static void main(String args[]) {
+        int matrix[][] = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 },
+            { 22, 56, 100 }
+        };
+        System.out.println("Number of rows: " + matrix.length);
+        System.out.println("Number of columns: " + matrix[0].length);
+    }
+}
