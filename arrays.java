6159fd71561b49b1da62fdaa1b839aa2577d5e2d
abhinavgitin <prabhudarsanpuri@gmail.com>
2025-09-14 16:12:11 +0530

    solved one uestion

--- arrays.java
+++ arrays.java
@@ -1,70 +1,55 @@
-// import java.util.*;
+import java.util.*;
 
-// class arrays {
-//     public static void main(String args[]) {
-//         Scanner sc = new Scanner(System.in);
+class arrays {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
 
-//         System.out.println("Enter the rows for the matrix");
-//         int rows = sc.nextInt();
-//         System.out.println("Enter the columns for the matrix");
-//         int clo = sc.nextInt();
-//         // 2 d array initialisation
-//         int[][] matrix = new int[rows][clo];
+        System.out.println("Enter the rows for the matrix");
+        int rows = sc.nextInt();
+        System.out.println("Enter the columns for the matrix");
+        int clo = sc.nextInt();
+        // 2 d array initialisation
+        int[][] matrix = new int[rows][clo];
 
-//         // taking input
-//         // for the rows first the outer loop
-//         for (int i = 0; i < rows; i++) {
-//             // for the columns in the inner loop
-//             System.out.println("enter the data for the row " + (i + 1));
-//             for (int j = 0; j < clo; j++) {
-//                 matrix[i][j] = sc.nextInt();
-//             }
-//         }
-//         System.out.println("enetr the data needed to be searched ");
-//         int element = sc.nextInt();
+        // taking input
+        // for the rows first the outer loop
+        for (int i = 0; i < rows; i++) {
+            // for the columns in the inner loop
+            System.out.println("enter the data for the row " + (i + 1));
+            for (int j = 0; j < clo; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("enetr the data needed to be searched ");
+        int element = sc.nextInt();
 
-//         boolean found = false; // to search weather we got the target as per our requirement or not we will use
-//                                // the boolean operation always
+        boolean found = false; // to search weather we got the target as per our requirement or not we will use
+                               // the boolean operation always
 
-//         for (int m = 0; m < rows; m++) {
-//             for (int n = 0; n < clo; n++) {
-//                 if (matrix[m][n] == element) {
-//                     System.out.println("data found at row " + (1 + m) + " and column at " + (1 + n));
-//                     found = true;
-//                     // break ;
-//                     // add this if you want to just find the first occurence;
-//                 } else {
-//                     continue;
-//                 }
-//             }
-//         }
-//         if (!found) {
-//             // i guess this is the best way to write a counditional statement
-//             System.out.println("not found ");
-//         }
-//         for (int i = 0; i < rows; i++) {
-//             // for the rows nesting
-//             for (int j = 0; j < clo; j++) {
-//                 // this is for the cloumns nesting
-//                 System.out.printf("%4d", matrix[i][j]);
-//             }
-//             System.out.println();
-//         }
-//         sc.close();
-//     }
-// }
-// this is find the length of the row and the length of the column
-
-
-class Solution {
-    public static void main(String[] args) {
-        int matrix[][] = {
-            { 1, 2, 3 },
-            { 4, 5, 6 },
-            { 7, 8, 9 },
-            { 22, 56, 100 }
-        };
-        System.out.println("Number of rows: " + matrix.length);
-        System.out.println("Number of columns: " + matrix[0].length);
+        for (int m = 0; m < rows; m++) {
+            for (int n = 0; n < clo; n++) {
+                if (matrix[m][n] == element) {
+                    System.out.println("data found at row " + (1 + m) + " and column at " + (1 + n));
+                    found = true;
+                    // break ;
+                    // add this if you want to just find the first occurence;
+                } else {
+                    continue;
+                }
+            }
+        }
+        if (!found) {
+            // i guess this is the best way to write a counditional statement
+            System.out.println("not found ");
+        }
+        for (int i = 0; i < rows; i++) {
+            // for the rows nesting
+            for (int j = 0; j < clo; j++) {
+                // this is for the cloumns nesting
+                System.out.printf("%4d", matrix[i][j]);
+            }
+            System.out.println();
+        }
+        sc.close();
     }
 }
