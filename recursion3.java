b124c5f37bf3c1b669be95045f6ab1037bc89c44
abhinavgitin <prabhudarsanpuri@gmail.com>
2025-10-03 23:00:08 +0530

    updated the recursion part 3

--- recursion3.java
+++ recursion3.java
@@ -14,19 +14,35 @@
 // }
 class recursion3 {
     public static void main(String[] args) {
+        // Demonstrate recursive sum
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = arraySum(arr, arr.length - 1);
+        System.out.println("Sum of array elements: " + sum);
+
+        // Demonstrate string permutations
         String str = "abc";
         permutationOfStr(str, "");
     }
-    public static void permutationOfStr( String str , String permutation ){
-        if ( str.length() == 0 ){
+
+    // Recursive sum of array elements
+    public static int arraySum(int[] arr, int idx) {
+        if (idx < 0) {
+            return 0;
+        }
+        return arr[idx] + arraySum(arr, idx - 1);
+    }
+
+    // Print all permutations of a string
+    public static void permutationOfStr(String str, String permutation) {
+        if (str.length() == 0) {
             System.out.println(permutation);
             return;
         }
-        for ( int i = 0 ; i < str.length() ; i++ ){
+        for (int i = 0; i < str.length(); i++) {
             char currChar = str.charAt(i);
-            String newStr = str.substring(0, i) + str.substring(i+1);
+            String newStr = str.substring(0, i) + str.substring(i + 1);
             permutationOfStr(newStr, permutation + currChar);
         }
-        return res;
+        // no return value for void method
     }
 }
