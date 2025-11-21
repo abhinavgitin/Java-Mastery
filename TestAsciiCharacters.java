e08d38b92a8fa54bc84bf275fd73ff8330afd7b7
abhinavgitin <prabhudarsanpuri987@gmail.com>
2025-11-21 22:50:16 +0530

    feat: to test the char wrt ascii

---
+++ TestAsciiCharacters.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class TestAsciiCharacters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        int ascii = (int) ch;
+        if ( ascii >= 65 && ascii <= 90) {
+            System.out.println("Uppercase Letter");
+        } else if (ascii >= 97 && ascii <= 122) {
+            System.out.println("Lowercase Letter");
+        } else if (ascii >= 48 && ascii <= 57) {
+            System.out.println("Digit");
+        } else {
+            System.out.println("Special Character");
+        }
+        sc.close();
+    }
+}
